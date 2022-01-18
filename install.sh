f1a065aad3f019999bcdee347247b666a1d3a722
armandoslopez97 <47642629+armandoslopez97@users.noreply.github.com>
2022-01-17 22:33:50 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 scp source/of/package/$1 ~/directory/to/install
-#scp armando@armandos-air:LinuxRepo/$1.png ~/Applications
-#gvfs-set-attribute ~/Applications/$ -t string1 metadata::custom-icon file://~/Applications/$1.png
 chmod u+x ~/directory/to/install/$1
-#sudo rm $1.png
+
